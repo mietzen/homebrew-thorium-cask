9062601f9f79c58144d1287c85d090ef48055da2
Nils Stein <social.nst@mailbox.org>
2023-03-04 11:08:28 +0100

    Multi Arch

--- Casks/thorium-chromium.rb
+++ Casks/thorium-chromium.rb
@@ -1,10 +1,20 @@
 cask "thorium-chromium" do
-  depends_on arch: :arm64
-  version "M109.0.5414.120-2"
-  sha256 "1d49f062c8806b282aadec0504bc7b756a2805c82cae41e675206acfae198610"
+  arch arm: "arm64", intel: "x86-64"
 
-  url "https://github.com/Alex313031/Thorium-Special/releases/download/#{version}/Thorium_MacOS_ARM64.dmg"
-  name "Chromium"
+  version "M109.0.5414.120-2"
+
+  on_intel do
+    arch_name "x64"
+    sha256 "d4ec6db1cd028041166dacb3a121406ce1619ebd965073600f9e43206b42f92b"
+  end
+  on_arm do
+    arch_name "ARM64"
+    sha256 "d4e8258a9e91618d2da82968285498f035f31e4912646479c225b1a4ae43675c"
+  end
+
+  url "https://github.com/Alex313031/Thorium-Special/releases/download/#{version}/Thorium_MacOS_#{arch_name}.dmg"
+
+  name "Thorium"
   desc "Thorium - The fastest browser on Earth."
   homepage "https://thorium.rocks/"
 
@@ -14,6 +24,13 @@ cask "thorium-chromium" do
     "freesmug-chromium"
   ]
 
-  app "Chromium.app"
+  app "Thorium.app"
+
+  zap trash: [
+    "~/Library/Application Support/Chromium",
+    "~/Library/Caches/Chromium",
+    "~/Library/Preferences/org.chromium.Chromium.plist",
+    "~/Library/Saved Application State/org.chromium.Chromium.savedState",
+  ]
 
 end
