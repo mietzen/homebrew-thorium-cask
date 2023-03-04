6723153be40a7b090607812a0f340b042c0692df
Nils Stein <social.nst@mailbox.org>
2023-03-04 13:15:40 +0100

    livecheck

--- Casks/thorium-chromium.rb
+++ Casks/thorium-chromium.rb
@@ -12,8 +12,12 @@ cask "thorium-chromium" do
   homepage "https://thorium.rocks/"
 
   livecheck do
-    url "https://github.com/Alex313031/Thorium-Special/releases"
-    regex(/MacOS [\w ]+ - (M\d+.\d+.\d+.\d+)<\/h2>\n$/i)
+    url "https://api.github.com/repos/Alex313031/Thorium-Special/releases"
+    regex(/MacOS [\w ]+ - (M\d+.\d+.\d+.\d+)/i)
+    strategy :json do |json, regex|
+      json["name"].select { |item| item["name"]&.match?(regex) }
+                      .map { |item| item["name"][regex, 1] }
+    end
   end
 
   app "Thorium.app"
