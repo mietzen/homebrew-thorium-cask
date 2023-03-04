6b8aac0c65763c2e5e5a7ed2759a89766c89007a
Nils Stein <social.nst@mailbox.org>
2023-03-04 11:49:09 +0100

    style check rev

--- Casks/thorium-chromium.rb
+++ Casks/thorium-chromium.rb
@@ -2,12 +2,17 @@ cask "thorium-chromium" do
   arch arm: "arm64", intel: "x86-64"
 
   version "M109.0.5414.120-2"
-  sha256 arm:   "1d49f062c8806b282aadec0504bc7b756a2805c82cae41e675206acfae198610",
-         intel: "d4ec6db1cd028041166dacb3a121406ce1619ebd965073600f9e43206b42f92b"
+
+  on_intel do
+    sha256 "d4ec6db1cd028041166dacb3a121406ce1619ebd965073600f9e43206b42f92b"
+  end
+  on_arm do
+    sha256 "1d49f062c8806b282aadec0504bc7b756a2805c82cae41e675206acfae198610"
+  end
 
   url "https://github.com/Alex313031/Thorium-Special/releases/download/#{version}/Thorium_MacOS_#{arch}.dmg"
   name "Thorium"
-  desc "Thorium - The fastest browser on Earth"
+  desc "Thorium - The fastest browser on Earth."
   homepage "https://thorium.rocks/"
 
   app "Thorium.app"
@@ -18,4 +23,5 @@ cask "thorium-chromium" do
     "~/Library/Preferences/org.chromium.Thorium.plist",
     "~/Library/Saved Application State/org.chromium.Thorium.savedState",
   ]
+
 end
