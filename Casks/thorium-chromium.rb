d7511714ed4680dfa6f8094b7285c45d6856e61e
Nils Stein <social.nst@mailbox.org>
2023-03-04 16:59:17 +0100

    livecheck

--- Casks/thorium-chromium.rb
+++ Casks/thorium-chromium.rb
@@ -1,3 +1,7 @@
+Homebrew.install_gem_setup_path! "pry"
+Homebrew.install_gem_setup_path! "pry-byebug", executable: "pry"
+require "pry-byebug"; binding.pry
+
 cask "thorium-chromium" do
   arch arm: "arm64", intel: "x86-64"
 
