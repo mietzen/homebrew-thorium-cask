beb626954c1419c60b8b45487c7ead83627079a0
Nils Stein <social.nst@mailbox.org>
2023-03-04 14:32:01 +0100

    livecheck

--- Casks/thorium-chromium.rb
+++ Casks/thorium-chromium.rb
@@ -15,7 +15,6 @@ cask "thorium-chromium" do
     url "https://api.github.com/repos/Alex313031/Thorium-Special/releases"
     regex(/.*MacOS.*/i)
     strategy :json do |json, regex|
-      puts(json)
       json[].select { |item| item["name"]&.match?(regex) }
                       .map { |item| item["tag_name"] }
     end
